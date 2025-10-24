0e61c79590bae318bf6615a893c3b2930a1a3d46
hariharan <qa-hariharan@twentyideas.com>
2025-10-25 00:33:10 +0530

    commit

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ console.log("Hello, World!");
 function add(a, b) {
   return a + b;
 }
+console.log("Hello, World!");
 
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(5, 10);
+console.log("The result is:", result);
 const result = add(5, 10);
 console.log("The result is:", result);
